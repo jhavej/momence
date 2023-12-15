01e24af9da9a7a96e2e6d3fb9fb0882fb13e9e7b
Jakub Havej <jakub.havej@gmail.com>
2023-12-15 19:03:28 +0100

    feat: express server

---
+++ api/index.ts
@@ -0,0 +1,50 @@
+import path from "node:path";
+import express, {
+  json,
+  static as static_,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(json());
+
+app.use(static_(path.join(__dirname, "build")));
+
+type AsyncHandlerFunction<T> = (
+  req: Request,
+  res: Response<T>,
+  next: NextFunction
+) => Promise<unknown>;
+
+const asyncHandler =
+  <T>(fn: AsyncHandlerFunction<T>) =>
+  (req: Request, res: Response<T>, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+
+const fetchExchangeRates = async () => {
+  const response = await fetch(
+    "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.text();
+  return data;
+};
+
+app.get(
+  "/exchangeRates",
+  asyncHandler<string>(async (_, res) => {
+    const response = await fetchExchangeRates();
+    return res.send(response);
+  })
+);
+
+app.listen(PORT, () => {
+  console.log("Start listening");
+});
